fac009f56f10c8909ab21a66f36a449b5fe1e65b
lexNwimue <nwimueprogress@gmail.com>
2022-07-23 21:11:54 +0100

    Fixed everything but nodemailer due to end of Google 'Less Secure App' Policy

--- server.js
+++ server.js
@@ -1,57 +1,57 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
+import ejs from "ejs";
 
-// __dirname as a global variable is not defined in ES6 module, hence the 
+// __dirname as a global variable is not defined in ES6 module, hence the
 // four lines of code below is to fix that issue
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.listen(5500, () => console.log('Listening for requests on port 5500...'));
-app.use(express.static('views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(5500, () => console.log("Listening for requests on port 5500..."));
+app.use(express.static("views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index.html', {root: __dirname});
-})
+app.get("/", (req, res) => {
+  res.render("index", { root: __dirname });
+});
 
-app.post('/contact', async (req, res) => {
+app.post("/contact", async (req, res) => {
+  const { name, email, subject } = req.body;
+  console.log(name, email, subject);
 
-    const { name, email, subject, } = req.body;
+  // There seems to be an issue with nodemailer making the 'from' (sender's) address
+  // my address, making me unable to identify the actual sender.
+  // Hence, I am using the message body to get the sender's name and email as shown in the next line
+  const message = `From ${name} - ${email}: \n ${req.body.message}`;
 
-    // There seems to be an issue with nodemailer making the 'from' (sender's) address
-    // my address, making me unable to identify the actual sender. 
-    // Hence, I am using the message body to get the sender's name and email as shown in the next line
-    const message = `From ${name} - ${email}: \n ${req.body.message}`;
+  if (process.env.EMAIL && process.env.PASSWORD) {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-    if(process.env.EMAIL && process.env.PASSWORD){
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD,
-            }
-        });
+    const mailOptions = {
+      to: process.env.EMAIL,
+      subject: subject,
+      text: message,
+    };
 
-        const mailOptions = {
-            to: process.env.EMAIL,
-            subject: subject,
-            text: message
-        };
-    
-        transporter.sendMail(mailOptions)
-            .then(info => {
-                console.log(info);
-                res.json({info});
-            })
-            .catch(err => console.err(err))
-    }
-})
+    transporter
+      .sendMail(mailOptions)
+      .then((info) => {
+        console.log(info);
+        res.json({ info });
+      })
+      .catch((err) => console.log(err));
+  }
+});
--- views/assets/js/myscripts.js
+++ views/assets/js/myscripts.js
@@ -1,46 +1,50 @@
-const form = document.getElementById('contact-form');
-const submitBtn = document.getElementById('submit-btn');
+const form = document.getElementById("contactForm");
+const submitBtn = document.getElementById("submit-btn");
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    submitBtn.disabled = true;
-    document.getElementById('loading').classList.remove('d-none');
-    document.getElementById('loading').classList.add('alert', 'alert-info', 'd-block');
-    const mail = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value,
-    }
-    
-    fetch("/contact", {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify(mail)
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("contact-name").value;
+  const email = document.getElementById("contact-email").value;
+  const subject = document.getElementById("contact-subject").value;
+  const message = document.getElementById("contact-message").value;
+
+  const loadingDiv = document.getElementById("loading");
+  const successDiv = document.getElementById("loading");
+  const errorDiv = document.getElementById("loading");
+
+  submitBtn.disabled = true;
+  loadingDiv.style.display = "block";
+  const mail = {
+    name: name,
+    email: email,
+    subject: subject,
+    message: message,
+  };
+
+  fetch("/contact", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(mail),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      loadingDiv.style.display = "none";
+      successDiv.style.display = "block";
+
+      setTimeout(() => {
+        successDiv.style.display = "none";
+        submitBtn.disabled = false;
+        name.value = email.value = subject.value = message.value = "";
+      }, 5000);
     })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('loading').classList.remove('d-block');
-            document.getElementById('loading').classList.add('d-none');
-            document.getElementById('sent-message').classList.remove('d-none');
-            document.getElementById('sent-message').classList.add('alert', 'alert-success', 'd-block');
-            setTimeout(() => {
-                document.getElementById('sent-message').classList.remove('d-block');
-                document.getElementById('sent-message').classList.add('d-none');
-                submitBtn.disabled = false;
-            }, 5000);
-        })
-        .catch(err => {
-            document.getElementById('sent-message').classList.remove('d-block');
-            document.getElementById('sent-message').classList.add('d-none');
-            document.getElementById('error-message').classList.remove('d-none');
-            document.getElementById('error-message').classList.add('alert', 'alert-danger', 'd-block');
-            setTimeout(() => {
-                document.getElementById('error-message').classList.remove('d-block');
-                document.getElementById('error-message').classList.add('d-none');
-                submitBtn.disabled = false;
-            }, 5000);
-        })
+    .catch((err) => {
+      errorDiv.style.display = "block";
+      loadingDiv.style.display = "none";
+      submitBtn.disabled = false;
+      setTimeout(() => {
+        errorDiv.style.display = "none";
+      }, 5000);
+    });
 });
